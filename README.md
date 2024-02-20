2b50f4a1403fd1b03d0cfd3f9597cde98e09bf61
Andrey-Fedosov <andrefedosov@gmail.com>
2024-02-20 23:16:10 +0300

    feature: add rating changing logic and css classes

--- README.md
+++ README.md
@@ -13,10 +13,39 @@
 ## code hints
 
 - for creating array of elements we can use this code:
-- ```
-  [...Array(props.numberOfStar)].map((_, index) => { return <FaStar key={index}/>})
   ```
+  {[...Array(props.numberOfStar)].map((_, index) => {return <FaStar key={index} size={50} />})}
+  ```
+- changing quantity of selected stars:
+
+  - for this i create 2 useState functions
+
+  ```
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  ```
+
+  - and 3 event listeners
+
+```
+  function clickHandler(currentIndex: number) {
+
+    setRating(currentIndex);
+  }
+  function mouseEnterHandler(currentIndex: number) {
+
+    setHover(currentIndex);
+  }
+  function mouseLeaveHandler() {
+
+    setHover(rating);
+  }
 
 ```
 
+- changing logic will be implemented via
+
+```
+  className={index <= (hover || rating) ? "active" : "inactive"}
+  // and relevant css classes
 ```
--- src/components/starRating/StarRating.tsx
+++ src/components/starRating/StarRating.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import "./style.css";
 
 export function StarRating(props: { numberOfStar: number }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function clickHandler(currentIndex: number) {
+    console.log(currentIndex);
+    setRating(currentIndex);
+  }
+  function mouseEnterHandler(currentIndex: number) {
+    console.log(currentIndex);
+    setHover(currentIndex);
+  }
+  function mouseLeaveHandler() {
+    console.log(rating);
+    setHover(rating);
+  }
+
   return (
     <div>
       {[...Array(props.numberOfStar)].map((_, index) => {
-        return <FaStar key={index} size={50} />;
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            size={50}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => clickHandler(index)}
+            onMouseEnter={() => {
+              mouseEnterHandler(index);
+            }}
+            onMouseLeave={() => {
+              mouseLeaveHandler();
+            }}
+          />
+        );
       })}
     </div>
   );
